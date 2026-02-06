b2a19a6504660b66be87b0054acd6f5400a5319b
opopopxxnot-spec <your-email@gmail.com>
2026-02-06 21:35:27 +0530

    Initial React app

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/reacttion/"
-})
+  base: "/reacttion/",   // 👈 repo name EXACT
+});
